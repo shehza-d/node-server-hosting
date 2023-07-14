3f63b7ca2f51e192a3f32f3cab65351432c577d1
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2023-07-14 10:29:46 +0500

    all routes added

--- server.mjs
+++ server.mjs
@@ -4,25 +4,27 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  console.log("🚀 ~ file: server.mjs:7 ~ app.get ~ req:", req);
-  res.send({ message: "Hello World! by Shehzad" });
+  console.log("🚀 ~ server.mjs:7 ~ app.get ~ req:", req);
+  res.send({ message: "♥️ Hello World! by Shehzad ❤️" });
 });
 
 app.get("/name/:myName", (req, res) => {
   const name = req.params.myName;
 
-  console.log("🚀 ~ file: server.mjs:13 ~ app.get ~ name:", name);
+  console.log("🚀 ~ server.mjs:13 ~ app.get ~ name:", name);
 
-  res.send({ message: `Hello World! by ${name}` });
+  res.send({ message: `Hello World! by ${name} 👋🏻` });
+});
+
+app.get("/first", (req, res) => {
+  res.send("This is my first Express server!!😁");
 });
 
 app.get("/waiting", (req, res) => {
   setTimeout(
-    () => res.send("Shehzad Responding late after 6s from HTTP Server"),
+    () => res.send("⏰ Shehzad Responding late after 6s from HTTP Server ⏰"),
     6000
   );
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+app.listen(port, () => console.log(`Example app listening on port ${port}`));
